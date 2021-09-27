13453e3a05bc3b2bd95d3b0a7e18f4b8bb8ce5bf
izaakwalz <iblack.xyz@gmail.com>
2021-09-27 02:16:16 +0100

    learning typescript

---
+++ learning-typeScript/src/01_basic-types.ts
@@ -0,0 +1,21 @@
+/**
+ * Basic Types
+ * 
+ * Variables and data types
+ * 
+ * @type string
+ * @type number
+ * @type boolean
+ * @description  I recommend to always specify types explicitly
+ */
+
+const dataTypes: string | number | boolean | any = null
+
+let id: number = 5;
+const hello: string = 'world';
+let world: string = 'world';
+let isPublished: boolean = true;
+let x: any = 'Hello'; // it could be any thing
+let ids: number[] = [1, 3, 4, 5, 6, 7, 8]; // array of numbers
+let arr: any[] = [1, true, 'hello']; // any allows you to add diffrent data types
+
---
+++ learning-typeScript/src/02_interface_and_objects.ts
@@ -0,0 +1,42 @@
+/**
+ * interfaces
+ * @interface
+ * it helps to describe entities. -
+ * Interface is an important entity inside typeScript
+ * which allows us to create objects with some properties,
+ * it is also a reserved word in typeScript
+ * interface can also have an optional property and also a readonly
+ * by default all properties inside interface are mandatory
+ */
+
+// postfix for all my interface
+interface UserInterface {
+ readonly id:number; // readonly
+ name: string;
+ age?: number;
+ getMessage(): string;
+}
+
+/**
+ * Objects in typeScripts
+ * @Objects
+ */
+
+// example 1
+// const user1: UserInterface = {
+// 	id: 1,
+// 	name: "jhon",
+// }
+
+const user2: UserInterface = {
+	id: 1,
+  name: 'Jack',
+  getMessage() {
+    return 'Hello';
+  },
+};
+// console.log(user2.getMessage());
+
+/*  it helps to describe entities 
+Interface is an important entity inside typeScript 
+which allows us to create objects with some properties */
