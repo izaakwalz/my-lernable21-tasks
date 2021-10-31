31f8e8686cf4eb4eeab98ad5278f335025ac4bf1
izaakwalz <iblack.xyz@gmail.com>
2021-10-31 06:32:47 +0100

    added filter books by query

--- book-lending-api/routes/user.route.js
+++ book-lending-api/routes/user.route.js
@@ -1,16 +1,34 @@
-const { getAll, lendBook } = require('../controllers/user.controller');
+const { getAllUsers, getOneUser, getUserBorrowedBooks, deleteUser } = require('../controllers/user.controller');
 
-route = {};
-
-module.exports = (data, callback) => {
-	const headers = ['post', 'get', 'put', 'delete'];
+const findAllUsers = (data, callback) => {
+	const headers = ['get', 'delete'];
 	if (headers.indexOf(data.method) === -1)
 		return callback(404, {
 			message: `Resource not found -> can not ${data.method.toUpperCase()} request to ${data.pathname}`,
 		});
-	route[data.method](data, callback);
+
+	if (data.method === 'get') getAllUsers(data, callback);
+	else data.method === 'delete' && deleteUser(data, callback);
 };
 
-route.get = (data, callback) => getAll(data, callback);
+const findOneUser = (data, callback) => {
+	const headers = ['get'];
+	if (headers.indexOf(data.method) === -1)
+		return callback(404, {
+			message: `Resource not found -> can not ${data.method.toUpperCase()} request to ${data.pathname}`,
+		});
+	if (!data.query.status) data.method === 'get' && getOneUser(data, callback);
+	else data.method === 'get' && getUserBorrowedBooks(data, callback);
+};
 
-route.put = (data, callback) => lendBook(data, callback);
+// const removeUser = (data, callback) => {
+// 	const headers = ['delete'];
+// 	if (headers.indexOf(data.method) === -1)
+// 		return callback(404, {
+// 			message: `Resource not found -> can not ${data.method.toUpperCase()} request to ${data.pathname}`,
+// 		});
+
+// 	data.method === 'delete' && deleteUser(data, callback);
+// };
+
+module.exports = { findAllUsers, findOneUser, removeUser };
